901174d18c36b723caccfd4bc38757c7ad1ad42a
victormartingil <victormartingil@gmail.com>
2023-02-09 00:15:02 +0100

    New endpoints to create admin and superadmin users

--- src/main/java/com/authentication/users/controller/UserController.java
+++ src/main/java/com/authentication/users/controller/UserController.java
@@ -3,6 +3,7 @@ package com.authentication.users.controller;
 import com.authentication.users.controller.mapper.UserControllerMapper;
 import com.authentication.users.models.domain.User;
 import com.authentication.users.service.UserService;
+import org.openapitools.client.model.RoleDto;
 import org.openapitools.client.model.UserQueryDto;
 import org.openapitools.client.model.UserResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,21 @@ public class UserController {
 
     @PostMapping("")
     public ResponseEntity<UserResponseDto> createUser(@RequestBody final UserQueryDto userQueryDto) {
+        userQueryDto.setRole(RoleDto.BASIC);
+        final User response = service.createUser(mapper.dtoToEntity(userQueryDto));
+        return new ResponseEntity<>(mapper.map(response), HttpStatus.OK);
+    }
+
+    @PostMapping("/admin")
+    public ResponseEntity<UserResponseDto> createUserAdmin(@RequestBody final UserQueryDto userQueryDto) {
+        userQueryDto.setRole(RoleDto.ADMIN);
+        final User response = service.createUser(mapper.dtoToEntity(userQueryDto));
+        return new ResponseEntity<>(mapper.map(response), HttpStatus.OK);
+    }
+
+    @PostMapping("/superadmin")
+    public ResponseEntity<UserResponseDto> createUserSuperAdmin(@RequestBody final UserQueryDto userQueryDto) {
+        userQueryDto.setRole(RoleDto.SUPERADMIN);
         final User response = service.createUser(mapper.dtoToEntity(userQueryDto));
         return new ResponseEntity<>(mapper.map(response), HttpStatus.OK);
     }
